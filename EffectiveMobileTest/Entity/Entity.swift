87f1e5303da4e801dcaafecbe7bf046da80f3c90
BanKokSik <bankok2bankok2@gmail.com>
2025-03-23 13:53:20 +0300

    Create model

---
+++ EffectiveMobileTest/Entity/Entity.swift
@@ -0,0 +1,16 @@
+//
+//  Entity.swift
+//  EffectiveMobileTest
+//
+//  Created by Nikita Chekmarev on 23.03.2025.
+//
+
+import UIKit
+
+struct Task {
+    let id: Int
+    let title: String
+    let description: String
+    let createdAt: Date
+    var isCompleted: Bool
+}
